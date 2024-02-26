2d690222107073d1afd748f49892278c7b89f414
Huijie Wei <huijiewei@outlook.com>
2024-02-26 20:46:16 +0800

    feat(utils): 增加 runIfFunction 工具函数

--- packages/utils/src/function/index.test.ts
+++ packages/utils/src/function/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "vitest";
-import { isFunction, type AnyFunction } from "./index";
+import { isFunction, runIfFunction, type AnyFunction } from "./index";
 
 describe("isFunction function", () => {
   test("should return true for a function", () => {
@@ -10,5 +10,19 @@ describe("isFunction function", () => {
   test("should return false for a non-function value", () => {
     const nonFunctionValue = "not a function";
     expect(isFunction(nonFunctionValue)).toBe(false);
+  });
+});
+
+describe("runMaybeFunction function", () => {
+  test("should run the function and return the result", () => {
+    const fn: AnyFunction<number, number> = (x) => x * 2;
+    const result = runIfFunction(fn, 5);
+    expect(result).toBe(10);
+  });
+
+  test("should return the value if it is not a function", () => {
+    const value = "not a function";
+    const result = runIfFunction(value);
+    expect(result).toBe(value);
   });
 });
--- packages/utils/src/function/index.ts
+++ packages/utils/src/function/index.ts
@@ -4,3 +4,7 @@ export type MaybeFunction<T, A extends unknown[] = []> = T | AnyFunction<A, T>;
 export const isFunction = <T = AnyFunction>(value: unknown): value is T => typeof value === "function";
 
 export const noop = () => {};
+
+export const runIfFunction = <R, A>(valueOrFunction: R | AnyFunction<A, R>, ...args: A[]): R => {
+  return isFunction<AnyFunction<A, R>>(valueOrFunction) ? valueOrFunction(...args) : valueOrFunction;
+};
