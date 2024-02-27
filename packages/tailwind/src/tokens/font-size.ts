93655ed7111a01fef5747ebc416101ec6db79fcd
Huijie Wei <huijiewei@outlook.com>
2024-02-27 10:08:09 +0800

    refactor(tailwind): 调整字体大小令牌

--- packages/tailwind/src/tokens/font-size.ts
+++ packages/tailwind/src/tokens/font-size.ts
@@ -1,5 +1,5 @@
 export const fontSize: Record<string, [fontSize: string, lineHeight: string]> = {
-  xs: ["0.75rem", "1rem"],
+  xs: ["0.813rem", "1rem"],
   sm: ["0.875rem", "1.25rem"],
   base: ["1rem", "1.5rem"],
   lg: ["1.125rem", "1.75rem"],
