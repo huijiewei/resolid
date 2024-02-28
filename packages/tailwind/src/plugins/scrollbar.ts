a0f69360c3c4dc1e2e6af257b582f8315595cccb
Huijie Wei <huijiewei@outlook.com>
2024-02-28 16:12:47 +0800

    refactor(tailwind): 调整滚动条大小

--- packages/tailwind/src/plugins/scrollbar.ts
+++ packages/tailwind/src/plugins/scrollbar.ts
@@ -8,21 +8,21 @@ export const scrollbar = (cssVarPrefix: string) => {
           borderWidth: "3px",
           borderStyle: "solid",
           borderColor: "transparent",
-          borderRadius: "5px",
+          borderRadius: "6px",
           backgroundClip: "padding-box",
           backgroundColor: `rgb(var(--${cssVarPrefix}-border-normal))`,
         },
       },
       ".scrollbar-base": {
         "&::-webkit-scrollbar": {
-          width: "12px",
-          height: "12px",
+          width: "11px",
+          height: "11px",
         },
       },
       ".scrollbar-thin": {
         "&::-webkit-scrollbar": {
-          width: "9px",
-          height: "9px",
+          width: "10px",
+          height: "10px",
         },
       },
     });
