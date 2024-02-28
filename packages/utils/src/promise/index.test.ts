cceb3e20668cc5f346229419898863000c4772b7
Huijie Wei <huijiewei@outlook.com>
2024-02-28 16:17:43 +0800

    feat(utils): 增加 debounce 工具函数

--- packages/utils/src/promise/index.test.ts
+++ packages/utils/src/promise/index.test.ts
@@ -1,5 +1,5 @@
-import { describe, expect, test } from "vitest";
-import { to, wait } from "./index";
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
+import { debounce, to, wait } from "./index";
 
 describe("wait function", () => {
   test("should resolve after the specified delay", async () => {
@@ -26,5 +26,32 @@ describe("to function", () => {
     const result = await to(promise);
 
     expect(result).toEqual([testError, undefined]);
+  });
+});
+
+describe("debounce function", () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  test("should debounce function calls", () => {
+    const fn = vi.fn();
+    const debouncedFn = debounce(fn, 100);
+
+    debouncedFn();
+    debouncedFn();
+    debouncedFn();
+
+    vi.advanceTimersByTime(50);
+
+    debouncedFn();
+
+    vi.advanceTimersByTime(100);
+
+    expect(fn).toHaveBeenCalledTimes(2);
   });
 });
--- packages/utils/src/promise/index.ts
+++ packages/utils/src/promise/index.ts
@@ -7,3 +7,21 @@ export const wait = (delay: number): Promise<void> => {
 export const to = async <T, E = Error>(promise: Promise<T>): Promise<[E, undefined] | [null, T]> => {
   return promise.then<[null, T]>((data) => [null, data]).catch<[E, undefined]>((error: E) => [error, undefined]);
 };
+
+export const debounce = (fn: () => void, delay: number): (() => void) => {
+  let timeoutId: NodeJS.Timeout;
+  let invoked = false;
+
+  return () => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    if (!invoked) {
+      fn();
+      (invoked = true) && setTimeout(() => (invoked = false), delay);
+    } else {
+      timeoutId = setTimeout(fn, delay);
+    }
+  };
+};
