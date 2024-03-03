6ee38a9ee760ea737e092111804a6f67ff15eb0d
Huijie Wei <huijiewei@outlook.com>
2024-03-03 08:20:38 +0800

    feat(react-ui): 增加 useClipboard Hook

--- packages/react-ui/src/hooks/index.ts
+++ packages/react-ui/src/hooks/index.ts
@@ -1,5 +1,6 @@
 export * from "./use-allow-hover";
 export * from "./use-callback-ref";
+export * from "./use-clipboard";
 export * from "./use-controllable-state";
 export * from "./use-disclosure";
 export * from "./use-isomorphic-effect";
---
+++ packages/react-ui/src/hooks/use-clipboard/index.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+export const useClipboard = (timeout = 2000, onError: (error: Error) => void = () => {}) => {
+  const [copied, setCopied] = useState(false);
+
+  const copy = async (source: string) => {
+    try {
+      await navigator.clipboard.writeText(source);
+    } catch (e) {
+      onError?.(e as Error);
+    }
+
+    setCopied(true);
+  };
+
+  useEffect(() => {
+    const id = copied ? setTimeout(() => setCopied(false), timeout) : null;
+
+    return () => {
+      id && clearTimeout(id);
+    };
+  }, [timeout, copied]);
+
+  return { copy, copied };
+};
