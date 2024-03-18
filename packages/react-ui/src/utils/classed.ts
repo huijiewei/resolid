fa1b4c7502147cd0e0f04ed5636abd9ba58221cc
Huijie Wei <huijiewei@outlook.com>
2024-03-18 20:29:24 +0800

    fix(react-ui): 更改 @tw-classed 引入方式

--- packages/react-ui/src/utils/classed.ts
+++ packages/react-ui/src/utils/classed.ts
@@ -1,2 +1,6 @@
-export { classed, type VariantProps } from "@tw-classed/core";
+import { createClassed } from "@tw-classed/core";
+
+export { type VariantProps } from "@tw-classed/core";
 export { clsx } from "clsx";
+
+export const { classed } = createClassed();
