e7f719d122c18780b51fa6840158ab900c17e74d
Huijie Wei <huijiewei@outlook.com>
2024-03-25 11:52:36 +0800

    feat(framework): 增加系统初始化填充功能

--- packages/framework/cli/commands/db.ts
+++ packages/framework/cli/commands/db.ts
@@ -1,23 +1,50 @@
+import { hashSync } from "@node-rs/bcrypt";
 import { Command } from "commander";
 import { spawn } from "node:child_process";
+import { exit } from "node:process";
+import { adminGroupTable, adminTable } from "../../src/modules/admin/schema";
 import type { CreateCommand } from "../index";
 
-export const dbCommand: CreateCommand = () => {
-  const db = new Command("db");
+export const dbCommand: CreateCommand = (db) => {
+  const cmd = new Command("db");
 
-  db.description("数据库操作");
+  cmd.description("数据库操作");
 
-  db.command("push")
+  cmd
+    .command("push")
     .description("数据库推送")
     .action(() => {
       spawn("drizzle-kit", ["push:pg"], { stdio: "inherit", shell: true });
     });
 
-  db.command("seed")
+  cmd
+    .command("seed")
     .description("数据库初始化填充")
-    .action(() => {
-      console.warn("数据库 seed");
+    .action(async () => {
+      await db
+        .insert(adminGroupTable)
+        .values([{ id: 1, name: "管理员" }])
+        .onConflictDoNothing({
+          target: adminGroupTable.id,
+        });
+
+      await db
+        .insert(adminTable)
+        .values({
+          id: 1,
+          adminGroupId: 1,
+          username: "admin",
+          nickname: "系统管理员",
+          password: hashSync("123456"),
+        })
+        .onConflictDoNothing({
+          target: adminTable.id,
+        });
+
+      console.log("数据库初始化填充成功");
+
+      exit();
     });
 
-  return db;
+  return cmd;
 };
