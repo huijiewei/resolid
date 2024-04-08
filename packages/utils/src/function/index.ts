5084494cd2bce498db9ae9864d4c110b3db8b3bd
Huijie Wei <huijiewei@outlook.com>
2024-04-08 21:01:02 +0800

    refactor(utils): 优化 function 类型定义

--- packages/utils/src/function/index.ts
+++ packages/utils/src/function/index.ts
@@ -1,9 +1,7 @@
 export type AnyFunction<A = unknown, R = unknown> = (...args: A[]) => R;
-export type MaybeFunction<T, A extends unknown[] = []> = T | AnyFunction<A, T>;
+export type MaybeFunction<R = unknown, A = unknown> = R | AnyFunction<A, R>;
 
 export const isFunction = <T = AnyFunction>(value: unknown): value is T => typeof value === "function";
-
-export const noop = () => {};
 
 export const runIfFunction = <R, A>(valueOrFunction: R | AnyFunction<A, R>, ...args: A[]): R => {
   return isFunction<AnyFunction<A, R>>(valueOrFunction) ? valueOrFunction(...args) : valueOrFunction;
